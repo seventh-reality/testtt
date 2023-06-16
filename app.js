caa4022c7861c507bb97d02d26367d372afde027
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2023-06-16 12:19:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -239,9 +239,7 @@ OX.init(config)
 
       document.getElementById("orange").addEventListener("click", () => {
        // changeCarColor(0xff2600);
-	<audio>
-            <source src="v.mp3">
-        </audio>     
+	    
 	   const gltfLoader = new GLTFLoader();
     gltfLoader.load("ETHOSs.glb", (gltf) => {
       car = gltf.scene;
