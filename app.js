5e6242af57b6917903f1d50f675181c45cac2a58
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-10 11:26:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,41 @@ function setupRenderer(rendererCanvas) {
 
   // Create an empty scene
   scene = new THREE.Scene();
+//Add lights some.
+	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2 );
+				hemiLight.color.setHSL( 0.6, 1, 0.6 );
+				hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+				hemiLight.position.set( 0, 50, 0 );
+				scene.add( hemiLight );
 
+				const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+				scene.add( hemiLightHelper );
+
+				//
+
+				const dirLight = new THREE.DirectionalLight( 0xffffff, 3 );
+				dirLight.color.setHSL( 0.1, 1, 0.95 );
+				dirLight.position.set( - 1, 1.75, 1 );
+				dirLight.position.multiplyScalar( 30 );
+				scene.add( dirLight );
+
+				dirLight.castShadow = true;
+
+				dirLight.shadow.mapSize.width = 2048;
+				dirLight.shadow.mapSize.height = 2048;
+
+				const d = 50;
+
+				dirLight.shadow.camera.left = - d;
+				dirLight.shadow.camera.right = d;
+				dirLight.shadow.camera.top = d;
+				dirLight.shadow.camera.bottom = - d;
+
+				dirLight.shadow.camera.far = 3500;
+				dirLight.shadow.bias = - 0.0001;
+
+				const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
+				scene.add( dirLightHelper );
   // Add some lights
   const hemisphereLight = new THREE.HemisphereLight(0xbbbbff, 0x444422);
   scene.add(hemisphereLight);
