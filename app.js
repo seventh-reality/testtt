e95ddee884927d99d87463e4433fc720427e4953
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-12 13:57:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -379,6 +379,19 @@ function createMarker(model, position) {
       });
 	 
  });
+function turnOnOff() {
+            let image = document.getElementById('Image');
+            if (image.src.match("ONbulb"))
+                image.src =
+"https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg";
+            else
+                image.src =
+"https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg";
+        }
+    </script>
+ 
+    <img id="Image" onclick="turnOnOff()" src=
+"https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg">	  
  
     // Subscribe to events
     OX.subscribe(OnirixSDK.Events.OnPose, function (pose) {
