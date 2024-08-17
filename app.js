bdc5df9be93406dbd292484e6d743b5c889685fc
seventhreality <104563775+seventh-reality@users.noreply.github.com>
2024-08-17 16:58:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -275,6 +275,7 @@ OX.init(config)
       placeCar();
       document.getElementById("transform-controls").style.display = "block";
       document.getElementById("color-controls").style.display = "block";
+      document.getElementById("tap-to-place").style.display = "block";
     });
 
    /* const scaleSlider = document.getElementById("scale-slider");
